c603bfba1d6859220fc517e5a9e1a674917ea25d
Akira Matsuda <ronnie@dio.jp>
2019-04-02 21:08:51 +0900

    Publish Model.stateful_enum

--- lib/stateful_enum/state_inspection.rb
+++ lib/stateful_enum/state_inspection.rb
@@ -4,9 +4,17 @@ require 'stateful_enum/machine'
 
 module StatefulEnum
   module StateInspection
+    extend ActiveSupport::Concern
+
+    class_methods do
+      def stateful_enum
+        @_stateful_enum
+      end
+    end
+
     # List of possible events from the current state
     def possible_events
-      events = self.class.instance_variable_get(:@_stateful_enum).events
+      events = self.class.stateful_enum.events
       events.select {|e| send("can_#{e.value_method_name}?") }
     end
   end
