38769429b42dbf2a261d9adc8127a0882be233cc
Akira Matsuda <ronnie@dio.jp>
2019-04-03 12:57:58 +0900

    The lists of names as lists of symbols

--- test/state_inspection_test.rb
+++ test/state_inspection_test.rb
@@ -5,12 +5,12 @@ require 'test_helper'
 class StateInspectionTest < ActiveSupport::TestCase
   def test_possible_events
     unassigned_bug = Bug.new status: :assigned, assigned_to: User.create!(name: 'matz')
-    assert_equal %w[resolve close], unassigned_bug.stateful_enum.possible_events.map(&:value_method_name)
+    assert_equal %i[resolve close], unassigned_bug.stateful_enum.possible_events.map(&:value_method_name)
   end
 
   def test_possible_event_names
     unassigned_bug = Bug.new status: :assigned, assigned_to: User.create!(name: 'matz')
-    assert_equal %w[resolve close], unassigned_bug.stateful_enum.possible_event_names
+    assert_equal %i[resolve close], unassigned_bug.stateful_enum.possible_event_names
   end
 
   def test_possible_states
