92078c536a2fd1d66306677f4136c8c43a139b6a
Akira Matsuda <ronnie@dio.jp>
2021-12-18 07:57:35 +0900

    Properly test with the default set of configurations of each Rails versions

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -10,6 +10,7 @@ require "stateful_enum"
 
 module Dummy
   class Application < Rails::Application
+    config.load_defaults "#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
